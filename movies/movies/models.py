04cefa66e842c31e78300ea813c1e7261c16e473
ingenziart <104985332+ingenziart@users.noreply.github.com>
2024-11-24 14:52:00 -0500

    rebuild mtv

--- movies/movies/models.py
+++ movies/movies/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class Movie(models.Model):
     title = models.CharField(max_length=200)
     year = models.IntegerField()
+
+
+def __st__(self):
+    return f"{self.title}from{self.year}"
--- movies/movies/views.py
+++ movies/movies/views.py
@@ -1,13 +1,8 @@
 from django.shortcuts import render
+from .models import Movie
 
-data = {
-    "movies": [
-        {"id": 5, "title": "jaws", "year": 2013},
-        {"id": 6, "title": "tom and jerry", "year": 2003},
-        {"id": 1, "title": "mr bean", "year": 2008},
-    ]
-}
+data = Movie.objects.all()
 
 
 def movie(request):
-    return render(request, "movies/movies.html", data)
+    return render(request, "movies/movies.html", {"movies": data})
